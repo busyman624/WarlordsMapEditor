2364eee26abbd9951da8bc1946e1d06fbd3cc7ee
Szutowicz <Krystian.Szutowicz@dnvgl.com>
2017-05-14 20:02:55 +0200

    first steps

--- WarlordsMapEditor/MainWindow.xaml.cs
+++ WarlordsMapEditor/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +24,60 @@ namespace WarlordsMapEditor
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = new Board(10, 10);
         }
     }
+
+    public class Board
+    {
+        int _rows;
+        int _columns;
+        List<Tile> _tiles = new List<Tile>();
+
+        public Board(int rows, int columns)
+        {
+            _rows = rows;
+            _columns = columns;
+            Random random = new Random();
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    _tiles.Add(new Tile()
+                    {
+                        Data = string.Format("Row {0}, Column {1}", r, c),
+                        Background = new SolidColorBrush(Color.FromArgb(255, (byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256)))
+                    });
+                }
+            }
+        }
+
+        public int Rows
+        {
+            get { return _rows; }
+            set { _rows = value; }
+        }
+
+        public int Columns
+        {
+            get { return _columns; }
+            set { _columns = value; }
+        }
+
+        public List<Tile> Tiles
+        {
+            get { return _tiles; }
+            set { _tiles = value; }
+        }
+    }
+
+    public class Tile
+    {
+        public string Data { get; set; }
+        public SolidColorBrush Background { get; set; }
+    }
+
 }
+
